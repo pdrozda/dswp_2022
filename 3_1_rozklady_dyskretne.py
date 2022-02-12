8a238c782c74032c9e35d62bc414f159ed671dae
Pawel <pdrozda@op.pl>
2022-02-12 09:32:03 +0100

    dodany rozklad dwumianowy

--- 3_1_rozklady_dyskretne.py
+++ 3_1_rozklady_dyskretne.py
@@ -1,10 +1,20 @@
 import scipy.stats as scs
 
 p = 0.5 # określenie prawdopodobieństwa sukcesu w rozkładzie bernoulliego oraz dumianowym
-date_size = 100000000 # wielkość geenerowanego zbioru danych
+date_size = 10000 # wielkość geenerowanego zbioru danych
+n = 10 # liczba powtórzeń eksperymentu w rozkładzie dwumianowym
+k = 4 # liczba sukcesów w rozkładzie dwumianowym
 
+#rozkład Bernoulliego - jeden eksperyment sukces z prawdopodobieństwem p
 rozklad_bernoulliego_dane = scs.bernoulli.rvs(p, size=date_size)
 print(rozklad_bernoulliego_dane)
 print('Średnia z wygenerowanych danych:', rozklad_bernoulliego_dane.mean())
 srednia = scs.bernoulli.stats(p, moments='m')
 print('Średnia teoretyczna:', srednia)
+sumaPrawdopodobieństw = 0
+for i in range(0,n+1):
+    prawdopodobienstwoDwumianowy = scs.binom.pmf(i, n, p)
+    print(prawdopodobienstwoDwumianowy)
+    sumaPrawdopodobieństw = sumaPrawdopodobieństw + prawdopodobienstwoDwumianowy
+
+print(sumaPrawdopodobieństw)
